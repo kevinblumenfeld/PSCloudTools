10006047442e91140b715539ceb0aa870ff56f9c
kevinblumenfeld <github@thenext.net>
2017-05-30 00:35:45 -0400

    Adjusted Hash Table for Mailbox Search Data to be Nested with InPlaceHoldIdentity as Key - Also added more data to report because of this change

--- PSInPlaceHold/Public/Find-Replace.ps1
+++ PSInPlaceHold/Public/Find-Replace.ps1
@@ -9,27 +9,34 @@ function Find-Replace() {
     }
     Process {
         $resultArray = @()
-        $Mailbox = Get-Mailbox -IncludeInactiveMailbox -ResultSize 10 | Select DisplayName, accountdisabled, IsInactiveMailbox, RecipientTypeDetails, UserPrincipalName, LitigationHoldEnabled, RetentionPolicy, RecoverableItemsQuota, InPlaceHolds
-        $InPlaceHold = Get-MailboxSearch -ResultSize unlimited | select name, inplaceholdidentity, Status, version, StartDate, EndDate
-        $MailboxProperties = $Mailbox | Get-Member -MemberType 'NoteProperty'| where {$_.Name -ne $FindParameter}| Select Name
-        $Id2NameLookup = @{}
-        foreach ($Hold in $InPlaceHold) {
-            $Id2NameLookup.add($hold.InPlaceHoldIdentity, $hold.name)
+        $findParameter = "InPlaceHolds"
+        $mailbox = Get-Mailbox -IncludeInactiveMailbox -ResultSize 10 | Select DisplayName, accountdisabled, IsInactiveMailbox, RecipientTypeDetails, UserPrincipalName, LitigationHoldEnabled, RetentionPolicy, RecoverableItemsQuota, InPlaceHolds
+        $mbxSearch = Get-MailboxSearch -ResultSize unlimited | select name, inplaceholdidentity, Status, version, StartDate, EndDate
+        $mailboxProperties = $mailbox | Get-Member -MemberType 'NoteProperty' | Select Name
+        
+        $hash = @{}
+        foreach ($sRow in $mbxSearch) {
+            foreach ($id in $sRow.InPlaceHoldIdentity) {
+                $hash[$id] = $sRow 
+            }
         }
-        $FindParameter = "InPlaceHolds"
-        foreach ($Row in $Mailbox) {           
-            ForEach ($HoldID in $Row.$FindParameter.split()) {
-                $ReplaceHash = @{}
-                $ReplaceHash['Name'] = $Id2NameLookup[$holdid]   
-                foreach ($field in $MailboxProperties.name) {
-                    $ReplaceHash[$field] = ($Row.$field) -join ","
+      
+        foreach ($row in $mailbox) {           
+            ForEach ($guid in $row.$findParameter) {
+                $mailboxHash = @{}
+                $mailboxHash['InPlaceHoldName'] = ($hash[$guid]).name
+                $mailboxHash['StatusofHold'] = ($hash[$guid]).Status
+                $mailboxHash['StartDate'] = ($hash[$guid]).StartDate
+                $mailboxHash['EndDate'] = ($hash[$guid]).EndDate                                                
+                foreach ($field in $mailboxProperties.name) {
+                    $mailboxHash[$field] = ($row.$field) -join ","
                 }  
-                $resultArray += [PScustomobject]$ReplaceHash        
+                $resultArray += [psCustomObject]$mailboxHash        
             }
         }
     }
     End {
-        ([PScustomobject]$resultArray)
-        [PScustomobject]$resultArray | Export-Csv "C:\scripts\test\333.csv" -nti
+        ([psCustomObject]$resultArray)
+        [psCustomObject]$resultArray | Export-Csv "C:\scripts\test\333.csv" -nti
     }
 }
