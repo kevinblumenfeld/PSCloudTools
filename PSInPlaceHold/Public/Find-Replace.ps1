9a0fd92a8ef1d2af71c1af40cf5619e8055cfc89
Thomas Laborde <thomas.laborde@gmail.com>
2017-05-30 10:41:53 +0900

    Mix both hashes

--- PSInPlaceHold/Public/Find-Replace.ps1
+++ PSInPlaceHold/Public/Find-Replace.ps1
@@ -1,4 +1,4 @@
-﻿function Find-Replace() {
+function Find-Replace() {
     [CmdletBinding()]
     Param
     (
@@ -9,7 +9,6 @@
     }
     Process {
         $resultArray  = @()
-        $Row = @()
         $Mailbox = Get-Mailbox -IncludeInactiveMailbox -ResultSize 10 | Select DisplayName, accountdisabled, IsInactiveMailbox, RecipientTypeDetails, UserPrincipalName, LitigationHoldEnabled, RetentionPolicy, RecoverableItemsQuota, InPlaceHolds
         $InPlaceHold = Get-MailboxSearch -ResultSize unlimited | select name, inplaceholdidentity, Status, version, StartDate, EndDate
         $MailboxProperties = $Mailbox | Get-Member -MemberType 'NoteProperty'| where {$_.Name -ne $FindParameter}| Select Name
@@ -18,17 +17,14 @@
             $Id2NameLookup.add($hold.InPlaceHoldIdentity, $hold.name)
         }
         $FindParameter = "InPlaceHolds"
-        foreach ($Row in $Mailbox) {
-            $MailboxHash = @{}
-            foreach ($field in $MailboxProperties.name) {
-                $MailboxHash[$field] = ($Row.$field) -join ","
-            }
-            $resultArray += [string]$MailboxHash
-            
+        foreach ($Row in $Mailbox) {           
             ForEach ($HoldID in $Row.$FindParameter.split()) {
                 $ReplaceHash = @{}
                 #$ReplaceHash['HoldID'] = $HoldId
-                $ReplaceHash['Name'] = $Id2NameLookup[$holdid]     
+                $ReplaceHash['Name'] = $Id2NameLookup[$holdid]   
+                foreach ($field in $MailboxProperties.name) {
+                    $ReplaceHash[$field] = ($Row.$field) -join ","
+                }  
                 $resultArray += [PScustomobject]$ReplaceHash        
             }
         }
