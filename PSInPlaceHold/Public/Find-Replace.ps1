96b7ca63f16b8156bc85d067db6907f770846fea
kevinblumenfeld <github@thenext.net>
2017-05-29 21:38:23 -0400

    Commit with 2 sets of members issue

--- PSInPlaceHold/Public/Find-Replace.ps1
+++ PSInPlaceHold/Public/Find-Replace.ps1
@@ -8,28 +8,33 @@
 
     }
     Process {
-        $resultArray = @()
-        $row = @()
-        $MailboxHold = Get-Mailbox -IncludeInactiveMailbox -ResultSize 10 | Select DisplayName, accountdisabled, IsInactiveMailbox, RecipientTypeDetails, UserPrincipalName, LitigationHoldEnabled, RetentionPolicy, RecoverableItemsQuota, InPlaceHolds
+        $resultArray  = @()
+        $Row = @()
+        $Mailbox = Get-Mailbox -IncludeInactiveMailbox -ResultSize 10 | Select DisplayName, accountdisabled, IsInactiveMailbox, RecipientTypeDetails, UserPrincipalName, LitigationHoldEnabled, RetentionPolicy, RecoverableItemsQuota, InPlaceHolds
         $InPlaceHold = Get-MailboxSearch -ResultSize unlimited | select name, inplaceholdidentity, Status, version, StartDate, EndDate
-        
-        $hash = @{}
+        $MailboxProperties = $Mailbox | Get-Member -MemberType 'NoteProperty'| where {$_.Name -ne $FindParameter}| Select Name
+        $Id2NameLookup = @{}
         foreach ($Hold in $InPlaceHold) {
-            $hash.add($hold.InPlaceHoldIdentity, $hold.name)
+            $Id2NameLookup.add($hold.InPlaceHoldIdentity, $hold.name)
         }
-        $hash
-        $FindParameter = "inplaceholds"
-        foreach ($row in $MailboxHold) {
-            ForEach ($HoldID in $row.$FindParameter.split()) {
+        $FindParameter = "InPlaceHolds"
+        foreach ($Row in $Mailbox) {
+            $MailboxHash = @{}
+            foreach ($field in $MailboxProperties.name) {
+                $MailboxHash[$field] = ($Row.$field) -join ","
+            }
+            $resultArray += [string]$MailboxHash
+            
+            ForEach ($HoldID in $Row.$FindParameter.split()) {
                 $ReplaceHash = @{}
-                $ReplaceHash['HoldID'] = $HoldId
-                $ReplaceHash['Name'] = $hash[$holdid]     
-                $resultArray += [PScustomobject]$ReplaceHash          
+                #$ReplaceHash['HoldID'] = $HoldId
+                $ReplaceHash['Name'] = $Id2NameLookup[$holdid]     
+                $resultArray += [PScustomobject]$ReplaceHash        
             }
         }
     }
     End {
-        # ([PScustomobject]$resultArray)
-        $resultArray | Export-Csv "C:\scripts\test\333.csv" -nti
+        ([PScustomobject]$resultArray)
+        [PScustomobject]$resultArray | Export-Csv "C:\scripts\test\333.csv" -nti
     }
 }
