595a5bfd93dc3773437ebf846d8dfb92c12952e6
kevinblumenfeld <github@thenext.net>
2017-06-11 21:05:59 -0400

    New Get-LAFolderSize more efficient with two commands and one folderscope

--- PSCloudTool/Public/Get-LAFolderSize.ps1
+++ PSCloudTool/Public/Get-LAFolderSize.ps1
@@ -1,35 +1,24 @@
 function Get-LAFolderSize {
-    <#
-.Synopsis
-   Short description
-.DESCRIPTION
-   Long description
-.EXAMPLE
-   Example of how to use this cmdlet
-.EXAMPLE
-   Another example of how to use this cmdlet
-#>
-    [CmdletBinding()]
-    Param
-    (
-        [Parameter(Mandatory = $true,
-            ValueFromPipeline = $true,
-            ValueFromPipelineByPropertyName = $true)]
-        [string[]] $upn
+    Param(
+        [Parameter(
+            Mandatory = $true,
+            ValueFromPipelineByPropertyName = $true,
+            ValueFromPipeline = $true)]
+        [String]$userprincipalname
     )
     Begin {
         $resultArray = @()
     }
     Process {
-            $stats = Get-MailboxFolderStatistics -Identity $_.userprincipalname |  where {$_.name -match "Recoverable Items|Top of Information Store"} | Select  @{name = "Size"; expression = {[math]::Round((($_.FolderAndSubfolderSize.ToString()).Split("(")[1].Split(" ")[0].Replace(",", "") / 1GB), 2)}}
-            Write-Output "UPN: $($_.userprincipalname)"
-            $Hash = [ordered]@{}
-            $Hash['UserPrincipalName'] = $_.userprincipalname
-            $Hash['RecoverableItemsSize'] = $stats[0].size
-            $Hash['TotalSize'] = $stats[1].size
-            $resultArray += [psCustomObject]$Hash  
+        $mboxsize = Get-MailboxStatistics $userprincipalname | select-Object -ExpandProperty TotalItemSize | Select  @{name = "Size"; expression = {[math]::Round((($_.Value.ToString()).Split("(")[1].Split(" ")[0].Replace(",", "") / 1GB), 2)}}
+        $reitemsize = (Get-MailboxFolderStatistics $userprincipalname -FolderScope RecoverableItems)[0] | select @{name = "Size"; expression = {[math]::Round((($_.FolderAndSubfolderSize.ToString()).Split("(")[1].Split(" ")[0].Replace(",", "") / 1GB), 2)}}
+        $hash = [Ordered]@{}
+        $hash['UserPrincipalName'] = $userprincipalname
+        $hash['RecoverableItemsSize'] = $reitemsize.size
+        $hash['TotalSize'] = $mboxsize.size
+        $resultArray += [psCustomObject]$hash 
     }
     End {
-        $resultArray 
+        return $resultArray
     }
 }
